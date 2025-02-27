282d9a9eb5f2f0e555d564db1af9c892fa0b04df
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-02-27 20:33:42 +0300

    yeni login testi yapıldı

--- src/gün01/Logintesti.java
+++ src/gün01/Logintesti.java
@@ -4,12 +4,11 @@ public class Logintesti {
     public static void main(String[] args) {
 
         //Bu bölüm Login Testi için yeni şifre modeline göre yazıldı.
-        System.out.println("Yeni Login testi ");
-
-      //yazdım
         System.out.println("Login testi 1");
 
-        System.out.println("sepet testi yapıldı");
+        System.out.println("sepet testi yapıldı.3günlük kod yazıldı");
+
+        System.out.println("yeni login testi yapıldı 7 gün ugraştım");
 
     }
 }
