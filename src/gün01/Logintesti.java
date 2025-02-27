10cbd3c305ad30f7abccb9be0854286ad6dde494
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-02-27 20:24:26 +0300

    login testi aşama 1(takstaki başlık jiradaki)

---
+++ src/gün01/Logintesti.java
@@ -0,0 +1,15 @@
+package gün01;
+
+public class Logintesti {
+    public static void main(String[] args) {
+
+        //Bu bölüm Login Testi için yeni şifre modeline göre yazıldı.
+        System.out.println("Yeni Login testi ");
+
+      //yazdım
+        System.out.println("Login testi 1");
+
+        System.out.println("sepet testi yapıldı");
+
+    }
+}
